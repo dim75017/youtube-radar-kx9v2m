944a6645720736c807a89495e4929d9cb02f2ce0
dim75017 <dim@lofigirl.com>
2026-07-23 23:40:35 +0200

    Spotify : invalider le cache du style 24 h

--- tests/test_spotify_24h_momentum.js
+++ tests/test_spotify_24h_momentum.js
@@ -4,6 +4,7 @@ const fs = require('node:fs');
 
 const source = fs.readFileSync('spotify/dashboard.js', 'utf8');
 const css = fs.readFileSync('spotify/dashboard.css', 'utf8');
+const index = fs.readFileSync('spotify/index.html', 'utf8');
 const tracksStart = source.indexOf('function renderOpps(){');
 const artistsStart = source.indexOf('function renderArtists(){');
 const playlistsStart = source.indexOf('function renderPlaylists(){');
@@ -22,5 +23,6 @@ assert.match(artists, /streamStackHtml\(g\.streams24,false,true\)/, 'Artists 24h
 assert.match(artists, /class="num stream-24h"/, 'Artists 24h table cell must be highlighted');
 assert.match(css, /\.stream-24h \.stream-number\{color:var\(--acc2\)\}/);
 assert.match(css, /\.buyout-estimate\{font-weight:600;color:var\(--text\)\}/);
+assert.match(index, /dashboard\.css\?v=20260723-24h-momentum-v1/);
 
 console.log('spotify 24h momentum UI: OK');
